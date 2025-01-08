baa1629356bbb9198a873b72f230895ebe1109ee
folka2134 <folka2134@gmail.com>
2025-01-08 15:30:45 +0000

    feat: Running game

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NumberGuessing.Controllers;
+using NumberGuessing.Services;
+using NumberGuessing.Views;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConsoleView view = new ConsoleView();
+            IGenerateNumber generator = new GenerateNumber();
+            var game = new GameController(view, generator);
+            game.Run();
+        }
+    }
+}
